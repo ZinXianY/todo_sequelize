f6c78af7c67cda510086c54f05d102f44b0bb14e
Zin <tyulkeds159357@gmail.com>
2021-11-07 16:21:53 +0800

    modify message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -21,15 +21,38 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
+  //取得註冊表單參數
+  const { name, email, password, confrimPassword } = req.body
+
+  //設定 Flash Message
+  const errors = []
+  if (!name || !email || !password || !confrimPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confrimPassword) {
+    errors.push({ message: '密碼與確認密碼不相符!' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confrimPassword
+    })
+  }
+
+  //檢查使用者是否註冊過
   User.findOne({ where: { email } }).then(user => {
+    //如註冊過退回原本畫面
     if (user) {
-      console.log('User already exists')
+      errors.push({ message: '這個 Email 已經註冊過了。' })
       return res.render('register', {
+        errors,
         name,
         email,
         password,
-        confirmPassword
+        confrimPassword
       })
     }
     return bcrypt
@@ -47,6 +70,7 @@ router.post('/register', (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
