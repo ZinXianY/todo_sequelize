384a86bcd58b4565b20594ea7747dcad7859ab44
Zin <tyulkeds159357@gmail.com>
2021-11-07 14:04:23 +0800

    add create todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -6,6 +6,21 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+//設定 New 頁面路由
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+//設定 Create 路由
+router.post('/', (req, res) => {
+  const userId = req.user._id
+  const name = req.body.name
+  return Todo.create({ name, userId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log('error'))
+})
+
+//設定 detail 路由
 router.get('/:id', (req, res) => {
   const UserId = req.user.id
   const id = req.params.id
