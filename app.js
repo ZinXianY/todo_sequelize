69eb47a43b5de5e7880aa488c438d3a089ef24dc
Zin <tyulkeds159357@gmail.com>
2021-11-07 13:55:38 +0800

    add message partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
 const routes = require('./routes0'
 )
@@ -25,9 +26,14 @@ app.use(methodOverride('_method'))
 
 usePassport(app)
 
+//設定 connect-flash
+app.use(flash())
+
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
